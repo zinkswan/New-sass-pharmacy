6ae0c144bcdc43502e8de4ebb4f456b544499196
elaatmanihamza <elaatmanihamza@gmail.com>
2023-07-03 13:27:44 +0200

    mes taches

---
+++ database/migrations/2023_07_02_120916_create_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->nullable();
+            $table->string('description')->nullable();
+            $table->string('name')->nullable();
+            $table->unsignedBigInteger('created_by');
+            $table->boolean('active')->default(true)->nullable();
+            $table->timestamps();
+
+            $table->foreign('created_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
