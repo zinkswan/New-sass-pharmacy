3f002947395fd34dadc3a8b31fcf95dec18ba3ec
zinkswan <lotera.app@gmail.com>
2023-07-09 00:58:38 +0100

    products & productsHistory tables

---
+++ database/migrations/2023_07_07_161510_products.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('productName');
+            $table->string('sku');
+            $table->integer('categoryId');
+            $table->integer('typeId');
+            $table->integer('quantity');
+            $table->integer('createdBy');
+            $table->integer('status');
+            $table->integer('tax');
+            $table->text('description');
+            $table->double('buyingPrice');
+            $table->double('sellingPrice');
+            $table->integer('active');
+            $table->timestamps();
+
+            $table->foreign('categoryId')->references('id')->on('categories');
+            $table->foreign('typeId')->references('id')->on('types');
+            $table->foreign('createdBy')->references('id')->on('users');
+            $table->foreign('status')->references('id')->on('statuses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
